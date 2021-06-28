a6cdff4d9fd6d307890405b15d6947178d32d7cd
vision0101 <shimcy2010@gmail.com>
2021-06-28 21:39:34 +0900

    UPDATE:질문과 대답에 대한 data.js 작성

---
+++ survey/js/data.js
@@ -0,0 +1,66 @@
+const qnaList = [
+    {
+      q: '1. 오늘 기분이 좋다.',
+      a: [
+        { answer: '매우 비동의', type: ['spicy', 'hot'] },
+        { answer: '비동의', type: ['spicy', 'hot'] },
+        { answer: '조금 비동의', type: ['spicy', 'hot' ] },
+        { answer: '중립', type: ['spicy', 'hot' ] },
+        { answer: '조금 동의', type: ['juicy', 'plain' ] },
+        { answer: '동의', type: ['juicy', 'plain' ] },
+        { answer: '매우 동의', type: ['juicy', 'plain' ] },
+      ]
+    },
+    {
+      q: '2. 오늘 기분이 우울하다.',
+      a: [
+        { answer: '매우 비동의', type: ['spicy', 'hot'] },
+        { answer: '비동의', type: ['spicy', 'hot'] },
+        { answer: '조금 비동의', type: ['spicy', 'hot' ] },
+        { answer: '중립', type: ['spicy', 'hot' ] },
+        { answer: '조금 동의', type: ['juicy', 'plain' ] },
+        { answer: '동의', type: ['juicy', 'plain' ] },
+        { answer: '매우 동의', type: ['juicy', 'plain' ] },
+      ]
+    },
+    {
+      q: '3. 오늘 기분이 기쁘다.',
+      a: [
+        { answer: '매우 비동의', type: ['spicy', 'hot'] },
+        { answer: '비동의', type: ['spicy', 'hot'] },
+        { answer: '조금 비동의', type: ['spicy', 'hot' ] },
+        { answer: '중립', type: ['spicy', 'hot' ] },
+        { answer: '조금 동의', type: ['juicy', 'plain' ] },
+        { answer: '동의', type: ['juicy', 'plain' ] },
+        { answer: '매우 동의', type: ['juicy', 'plain' ] },
+      ]
+    },
+    {
+      q: '4. 분노가 느껴진다.',
+      a: [
+        { answer: '매우 비동의', type: ['spicy', 'hot'] },
+        { answer: '비동의', type: ['spicy', 'hot'] },
+        { answer: '조금 비동의', type: ['spicy', 'hot' ] },
+        { answer: '중립', type: ['spicy', 'hot' ] },
+        { answer: '조금 동의', type: ['juicy', 'plain' ] },
+        { answer: '동의', type: ['juicy', 'plain' ] },
+        { answer: '매우 동의', type: ['juicy', 'plain' ] },
+      ]
+    },
+    {
+      q: '5. 오늘 기분이 우울하다.',
+      a: [
+        { answer: '매우 비동의', type: ['spicy', 'hot'] },
+        { answer: '비동의', type: ['spicy', 'hot'] },
+        { answer: '조금 비동의', type: ['spicy', 'hot' ] },
+        { answer: '중립', type: ['spicy', 'hot' ] },
+        { answer: '조금 동의', type: ['juicy', 'plain' ] },
+        { answer: '동의', type: ['juicy', 'plain' ] },
+        { answer: '매우 동의', type: ['juicy', 'plain' ] },
+      ]
+    }
+  ]
+  
+  const infoList = [
+    
+  ]
---
+++ survey/js/start.js
@@ -0,0 +1,6 @@
+const qna = document.querySelector("#test-question");
+
+function q1(){
+    var q = document.querySelector('.q1Box');
+    q.innerHTML = qnaList[0].q
+}
