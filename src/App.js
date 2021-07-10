a4d0fd4dce66593f191365b3867d6ebfb2df58fe
eastflag <eastflag@gmail.com>
2021-07-10 17:08:05 +0900

    초기 설정

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import Emotion from "./pages/emotion/Emotion";
+import Main from "./pages/Main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Main}></Route>
+          <Route path="/emotion" component={Emotion}></Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
