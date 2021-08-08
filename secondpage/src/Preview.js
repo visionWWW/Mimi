b5a89e0c905b5ef910dded91da79abdf8c4cb5d6
knyoon <kim_na_yoon@naver.com>
2021-08-09 02:24:47 +0900

    update.

--- secondpage/src/Preview.js
+++ secondpage/src/Preview.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class Preview extends Component {
     render(){
         return(
-                <h1>Hello React World!!</h1>
+        <div>
+            <p>평점: </p>
+            <p>메뉴: </p>
+            <p>가격: </p> 
+            <p>#키워드 #키워드 #키워드</p> 
+        </div>
         )
     }
 }
