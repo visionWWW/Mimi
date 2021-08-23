270a847ac89ab351fabac46d0d742036d969ce57
knyoon <kim_na_yoon@naver.com>
2021-08-23 21:02:46 +0900

    update: 네비게이션바 src 주소 수정

--- src/pages/allresults/Result.jsx
+++ src/pages/allresults/Result.jsx
@@ -11,7 +11,7 @@ function Result() {
       <div className="App">
         <header>
         <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="../">
             <img
               src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F3a2c92f5-f33b-4df5-84b5-3ce2ebdd0c0f%2F_-001_(1).png?table=block&id=360799d6-74b6-44a8-b131-ebc234ef5f06&spaceId=4145db40-ad1d-42f4-9f54-6e512b13bafb&width=580&userId=fc5b8e01-4d70-4ab8-8078-e67683ebc863&cache=v2"
               width="100"
@@ -23,7 +23,7 @@ function Result() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="../survey/src/survey.html">검사하기</Nav.Link>
-              <Nav.Link href="">감정진단결과</Nav.Link>
+              <Nav.Link href="../emotion">감정진단결과</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="../restaurant">음식점 보기</Nav.Link>
@@ -31,7 +31,7 @@ function Result() {
           </Navbar.Collapse>
 
           <Navbar.Collapse className="justify-content-end">
-            <Nav.Link href="#pricing" className="ha">회원가입</Nav.Link>
+            <Nav.Link href="../review" className="ha">회원가입</Nav.Link>
           </Navbar.Collapse></Navbar>
           <h2>감정결과</h2>
           <Row xs={1} md={2} className="g-4">
--- src/pages/restaurant/Restaurtant.jsx
+++ src/pages/restaurant/Restaurtant.jsx
@@ -17,7 +17,7 @@ function App() {
     <div className="App">
       <header>
       <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
-          <Navbar.Brand href="#home">
+          <Navbar.Brand href="../">
             <img
               src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F3a2c92f5-f33b-4df5-84b5-3ce2ebdd0c0f%2F_-001_(1).png?table=block&id=360799d6-74b6-44a8-b131-ebc234ef5f06&spaceId=4145db40-ad1d-42f4-9f54-6e512b13bafb&width=580&userId=fc5b8e01-4d70-4ab8-8078-e67683ebc863&cache=v2"
               width="100"
@@ -29,7 +29,7 @@ function App() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
               <Nav.Link href="../survey/src/survey.html">검사하기</Nav.Link>
-              <Nav.Link href="">감정진단결과</Nav.Link>
+              <Nav.Link href="../emotion">감정진단결과</Nav.Link>
             </Nav>
             <Nav>
               <Nav.Link href="../restaurant">음식점 보기</Nav.Link>
@@ -37,7 +37,7 @@ function App() {
           </Navbar.Collapse>
 
           <Navbar.Collapse className="justify-content-end">
-            <Nav.Link href="#pricing" className="ha">회원가입</Nav.Link>
+            <Nav.Link href="../review" className="ha">리뷰 작성하기</Nav.Link>
           </Navbar.Collapse></Navbar>
 
 
