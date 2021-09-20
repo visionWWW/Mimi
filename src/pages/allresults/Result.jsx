d82dab762246405350109b328fd01a462b0bd1d0
knyoon <kim_na_yoon@naver.com>
2021-09-20 23:20:38 +0900

    update: 카드 뒷면 뒤집기 적용

--- src/pages/allresults/Result.jsx
+++ src/pages/allresults/Result.jsx
@@ -9,91 +9,175 @@ import './result.css';
 
 function Result() {
     return (
-      <div className="App">
-        <header>
-        <NaviagtionBar/>
+        <div className="App">
+            <header>
+                <NaviagtionBar />
 
-          <h2>감정결과</h2>
-          <div className="result">
-          <Row>
+                <h2>감정결과</h2>
+                <div className="result">
+                    <Row>
+                        <Col xs={6} md={4}>
+
+                            <div className="flip-container" ontouchstart="this.classList.toggle('hover');" style={{ height: '32rem', width: '18rem', display: 'inline-block' }}>
+                            <div className="flipper">
+                                <div className="front">
+                                    <Card className="card" style={{height: '32rem', width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/100.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                                지금의 기쁨을 유지할 수 있도록 돕는 음식을 먹으면서 더 큰 행복감을 누리세요!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+
+                                </div>
+                                <div className="back">
+                                    <Card className="card" style={{height: '32rem', width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/back_100.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body> </Card>
+                            </div></div></div>
+                        </Col>
+
                 <Col xs={6} md={4}>
-                <Card className="card">
-                    <Card.Img variant="top" src="/images/100.png" />
-                    <Card.Body>
-                    <Card.Title>기쁨 100%</Card.Title>
-                    <Card.Text>
-                    지금의 기쁨을 유지할 수 있도록 돕는 음식을 먹으면서 더 큰 행복감을 누리세요! 
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                    </Card>
+                
+                <div className="flip-container" ontouchstart="this.classList.toggle('hover');" style={{ width: '18rem', display: 'inline-block' }}>
+                            <div className="flipper">
+                                <div className="front">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/80.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            더 큰 기쁨을 누릴 자격이 있는 당신입니다! 음식을 통해 더 큰 기쁨을 맛보아요!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+
+                                </div>
+                                <div className="back">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/back_80.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body> </Card>
+                            </div></div></div>
                 </Col>
 
                 <Col xs={6} md={4}>
-                <Card>
-                    <Card.Img variant="top" src="/images/80.png" />
-                    <Card.Body>
-                    <Card.Title>기쁨 80%</Card.Title>
-                    <Card.Text>
-                    더 큰 기쁨을 누릴 자격이 있는 당신입니다! 음식을 통해 더 큰 기쁨을 맛보아요!
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <div className="flip-container" ontouchstart="this.classList.toggle('hover');" style={{ width: '18rem', display: 'inline-block' }}>
+                            <div className="flipper">
+                                <div className="front">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/65.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            정서적으로 안정되고 편안한 상태이시군요~
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+
+                                </div>
+                                <div className="back">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/back_65.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body> </Card>
+                            </div></div></div>
                 </Col>
 
                 <Col xs={6} md={4}>
-                <Card>
-                    <Card.Img variant="top" src="/images/65.png" />
-                    <Card.Body>
-                    <Card.Title>기쁨 65%</Card.Title>
-                    <Card.Text>
-                    정서적으로 안정되고 편안한 상태이시군요~
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <div className="flip-container" ontouchstart="this.classList.toggle('hover');" style={{ width: '18rem', display: 'inline-block' }}>
+                            <div className="flipper">
+                                <div className="front">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/50.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            맛있는 음식 먹고 기운내자고요! 화이팅!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+
+                                </div>
+                                <div className="back">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/back_50.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body> </Card>
+                            </div></div></div>
                 </Col>
 
                 <Col xs={6} md={4}>
-                <Card>
-                    <Card.Img variant="top" src="/images/50.png" />
-                    <Card.Body>
-                    <Card.Title>기쁨 50%</Card.Title>
-                    <Card.Text>
-                    맛있는 음식 먹고 기운내자고요! 화이팅!
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                <div className="flip-container" ontouchstart="this.classList.toggle('hover');" style={{ width: '18rem', display: 'inline-block' }}>
+                            <div className="flipper">
+                                <div className="front">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/25.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            고생했어요. 이 음식이 당신에게 위로가 됐으면 좋겠어요!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+
+                                </div>
+                                <div className="back">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/back_25.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body> </Card>
+                            </div></div></div>
                 </Col>
 
                 <Col xs={6} md={4}>
-                <Card>
-                    <Card.Img variant="top" src="/images/25.png" />
-                    <Card.Body>
-                    <Card.Title>고생했어요. 이 음식이 당신에게 위로가 됐으면 좋겠어요!</Card.Title>
-                    <Card.Text>
-                    폭신한 구름빵에 뒹구는 순간처럼 행복한 당신! 
-음식을 먹으면서 누리는 기쁨 또한 느껴보아요! ♡
-이 음식을 먹으면 행복은 2배가 될거예요!
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
-                </Col>
+                <div className="flip-container" ontouchstart="this.classList.toggle('hover');" style={{ width: '18rem', display: 'inline-block' }}>
+                            <div className="flipper">
+                                <div className="front">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/10.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            힘들죠? 오늘도 힘내줘서 고마워요 맛있는 음식 먹고 살아있음을 느껴봐요!
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
 
-                <Col xs={6} md={4}>
-                <Card>
-                    <Card.Img variant="top" src="/images/10.png" />
-                    <Card.Body>
-                    <Card.Title>힘들죠? 오늘도 힘내줘서 고마워요 맛있는 음식 먹고 살아있음을 느껴봐요!</Card.Title>
-                    <Card.Text>
-                    
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                    </Card.Body>
-                </Card>
+                                </div>
+                                <div className="back">
+                                    <Card className="card" style={{ width: '18rem', display: 'inline-block' }}>
+                                        <Card.Img variant="top" src="/images/back_10.png" />
+                                        <Card.Body>
+                                            <Card.Title></Card.Title>
+                                            <Card.Text>
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body> </Card>
+                            </div></div></div>
                 </Col>
             </Row>
             </div>
