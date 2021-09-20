017aa31dcf6a8a8bcf022782c42b1ef71ea38744
최숙 <langgel1222@gmail.com>
2021-09-20 20:47:14 +0900

    UPDATE : style들 css 파일로 리팩토링, Navbar 변경

--- src/pages/young/Young.jsx
+++ src/pages/young/Young.jsx
@@ -1,97 +1,35 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React, {Component} from 'react';
+import Navbar from '../navbar';
 import Button from "react-bootstrap/Button";
 import Chart from './chart.js'
-import buttonCheck from './buttonCheck.js';
-import { Component } from "react";
-import { height } from 'dom-helpers';
+import './SurveyStyle.css';
 
 class Young extends Component {
   render(){
-	const totallWrap={
-		width: '800px',
-		margin: 'auto'
-	}
-
-	const introWrap = {
-		backgroundColor: '#FFE650',
-		lineHeight: '20px',
-		width: '800px',
-		margin: 'auto'
-	}
-	const surveyWrap = {
-		
-		lineHeight: '100px',
-		width:'100px',
-		margin: 'auto'
-	}
-	
-	const main = {
-		backgroundColor: 'whitesmoke',
-		width: '80%',
-		textAlign: 'center',
-		borderRadius: '20px'
-	  }
-
-	  const satis_qna = {
-		  backgroundColor : 'whitesmoke',
-		  height: '300px',
-		  margin: 'auto',
-		  textAlign:"center", 
-		  marginTop:"50px"
-	  }
   return (
-	  
-	  <div className="container">
-		  <Navbar collapseOnSelect expand="lg" bg="white" variant="light">
-          <Navbar.Brand href="../">
-            <img
-              src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F3a2c92f5-f33b-4df5-84b5-3ce2ebdd0c0f%2F_-001_(1).png?table=block&id=360799d6-74b6-44a8-b131-ebc234ef5f06&spaceId=4145db40-ad1d-42f4-9f54-6e512b13bafb&width=580&userId=fc5b8e01-4d70-4ab8-8078-e67683ebc863&cache=v2"
-              width="100"
-              height="100"
-              className="d-inline-block align-top"
-              alt="profile"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="../surveyIntro">검사하기</Nav.Link>
-              <Nav.Link href="../emotion">감정진단결과</Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="../restaurant">음식점 보기</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
+  	<>
+		<Navbar></Navbar>
 
-          <Navbar.Collapse className="justify-content-end">
-            <Nav.Link href="../review" className="ha">리뷰 작성하기</Nav.Link>
-          </Navbar.Collapse></Navbar>
-	  
-		  <div class="container">
-  </div>
-	  
         <div className="row mt-5">
-          <div className="col-sm-12" style={totallWrap}>
-			<div style={satis_qna}>
+          <div className="col-sm-12">
+			<div className="satis_qna">
 			<label >
 				만족감질문입니다
 			</label>
 			<form>
-				<label style={introWrap}>
+				<label className="intro">
 				1. 나는 내 삶의 개인적인 측면에 대해 만족한다.
 				</label>
 				<Chart></Chart>
 				</form>
 				<form>
-				<label style={introWrap}>
+				<label className="intro">
 				2. 나는 내 삶의 관계적 측면에 대해 만족한다. 
 				</label>
 				<Chart></Chart>
 				</form>
 				<form>
-				<label style={introWrap}>
+				<label className="intro">
 				3. 나는 내가 속한 집단에 대해 만족한다. 
 				</label>
 				<Chart></Chart>
@@ -102,38 +40,38 @@ class Young extends Component {
 				감정질문입니다
 			</label>
 				<form>
-				<label style={introWrap}>
+				<label className="intro">
 				1. 나는 현재 즐거운 감정을 느끼고 있다. 
 				</label>
 				<Chart></Chart>
 				</form>
 			  <form >
-				<label style={introWrap}>
+				<label className="intro">
 				2. 나는 현재 편안한 감정을 느끼고 있다. 
 				</label>
 				<Chart></Chart>
 				</form>
 			  <form>
-				<label style={introWrap}>
+				<label className="intro">
 				3. 나는 현재 행복한 감정을 느끼고 있다. 
 				</label>
 				<Chart></Chart>
 				</form>
 			  <form>
-				<label style={introWrap}>
+				<label className="intro">
 				4. 나는 현재 부정적인 감정을 느끼고 있다. 
 				</label>
 				<Chart></Chart>
 				</form>
 			  <form>
-				<label style={introWrap}>
+				<label className="intro">
 				5. 나는 현재 짜증나는 감정을 느끼고 있다. 
 				</label>
 				<Chart></Chart>
 				</form>
 
 				<form>
-				<label style={introWrap}>
+				<label className="intro">
 				6. 나는 현재 무기력한 감정을 느끼고 있다. 
 				</label>
 				<Chart></Chart>
@@ -144,7 +82,7 @@ class Young extends Component {
           </div>
         </div>
 
-      </div>
+	</>
   );
 };
 }
--- src/pages/young/chart.js
+++ src/pages/young/chart.js
@@ -1,111 +1,108 @@
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
-import React, { Component, useCallback } from "react";
+import React, {Component} from "react";
 
 const LabelWrap = {
 	width: '1000px',
 	margin: 'auto',
-	
+
 }
 class Chart extends Component {
 	constructor() {
 		super();
 		this.state = {
-		  name: "React",
-		  number : 0
+			name: "React",
+			number : 0
 		};
 		this.onChangeValue = this.onChangeValue.bind(this);
-	  }
-	
-	  onChangeValue(event) {
+	}
+
+	onChangeValue(event) {
 		console.log(event.target.value);
 		this.state = {
 			number : 1
-	  };}
-	  
+		};}
+
 
 	render(){
 		const {number} = this.state;
 		return(
 			<>
-			
-		<div onChange={this.onChangeValue}>
-			<h1>{number}</h1> {/*+1 버튼 누를 때 마다 값이 증가하는걸 하고 싶었는데 누를때마다 새로고침 됨..*/}
-			<button onClick={()=>{console.log("선택한 "+{number})}}></button> {/* 콘솔에 뜨는 옵션value 확인하려고 만든건데 object라고 뜸 */}
-		<button onClick={()=>{this.setState({number : number +1});}}> +1 </button>
-		<label onChange={this.onChangeValue1}sytle={LabelWrap}>
-			<input 
-			type="radio"
-			name="react-tips"
-			value="option1"
-			className="form-check-input-1"
-			/>
-			1
-		</label>
 
-		<label>
-			<input
-			type="radio"
-			name="react-tips"
-			value="option2"
-			className="form-check-input-1"
-			/>
-			2     
-		</label>
-					
-		<label>
-			<input
-			type="radio"
-			name="react-tips"
-			value="option3"
-			className="form-check-input-1"
-			/>
-			3     
-		</label>
-					
-		<label>
-			<input
-			type="radio"
-			name="react-tips"
-			value="option4"
-			className="form-check-input-1"
-			/>
-			4     
-		</label>
-					
-		<label>
-			<input
-			type="radio"
-			name="react-tips"
-			value="option5"
-			className="form-check-input-1"
-			/>
-			5     
-		</label>
-					
-		<label>
-			<input
-			type="radio"
-			name="react-tips"
-			value="option6"
-			className="form-check-input-1"
-			/>
-			6     
-		</label>
-					
-		<label>
-			<input
-			type="radio"
-			name="react-tips"
-			value="option7"
-			className="form-check-input-1"
-			/>
-			7    
-		</label>
+				<div onChange={this.onChangeValue}>
+					<h1>{number}</h1> {/*+1 버튼 누를 때 마다 값이 증가하는걸 하고 싶었는데 누를때마다 새로고침 됨..*/}
+					<button onClick={()=>{console.log("선택한 "+{number})}}></button> {/* 콘솔에 뜨는 옵션value 확인하려고 만든건데 object라고 뜸 */}
+					<button onClick={()=>{this.setState({number : number +1});}}> +1 </button>
+					<label onChange={this.onChangeValue1}sytle={LabelWrap}>
+						<input
+							type="radio"
+							name="react-tips"
+							value="option1"
+							className="form-check-input-1"
+						/>
+						1
+					</label>
 
-		</div>
-	</>
+					<label>
+						<input
+							type="radio"
+							name="react-tips"
+							value="option2"
+							className="form-check-input-1"
+						/>
+						2
+					</label>
+
+					<label>
+						<input
+							type="radio"
+							name="react-tips"
+							value="option3"
+							className="form-check-input-1"
+						/>
+						3
+					</label>
+
+					<label>
+						<input
+							type="radio"
+							name="react-tips"
+							value="option4"
+							className="form-check-input-1"
+						/>
+						4
+					</label>
+
+					<label>
+						<input
+							type="radio"
+							name="react-tips"
+							value="option5"
+							className="form-check-input-1"
+						/>
+						5
+					</label>
+
+					<label>
+						<input
+							type="radio"
+							name="react-tips"
+							value="option6"
+							className="form-check-input-1"
+						/>
+						6
+					</label>
+
+					<label>
+						<input
+							type="radio"
+							name="react-tips"
+							value="option7"
+							className="form-check-input-1"
+						/>
+						7
+					</label>
+
+				</div>
+			</>
 		);
 	}
 }
