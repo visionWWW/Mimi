dfde76a6d457380a344ecb3e58804e72906ef605
vision0101 <shimcy2010@gmail.com>
2021-09-21 18:51:40 +0900

    Update: 설문조사 페이지 Card로 디자인 변경

--- src/pages/young/Young.jsx
+++ src/pages/young/Young.jsx
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
+import { Card } from 'antd';
+import 'antd/dist/antd.css';
 import Navbar from '../navbar';
 import Button from "react-bootstrap/Button";
 import './SurveyStyle.css';
 import Footer from "../footer/Footer";
+
 
 class Young extends Component {
 	state = {
@@ -41,13 +44,8 @@ class Young extends Component {
 	<Navbar></Navbar>
 	<div className="row mt-5">
 	<div className="col-sm-12">
-	<div className="satis_qna">
-	<label >
-		만족감질문입니다
-	</label>
-	<h1>결과 1 합 : {result1}</h1>
-		<form>
-			<h3>1. 나는 내 삶의 개인적인 측면에 대해 만족한다.</h3>
+	<Card title="만족감질문입니다" style={{marginBottom:'30px'}}>
+    <Card type="inner" title="1. 나는 내 삶의 개인적인 측면에 대해 만족한다.">
 			<label>
 				1 <input type= "radio" value="1" name="satis_1" checked={satis_1 === "1"} onChange={this.onChange}/>
 			</label>
@@ -69,10 +67,8 @@ class Young extends Component {
 			<label>
 				7 <input type= "radio" value="7" name="satis_1" checked={satis_1 === "7"} onChange={this.onChange}/>
 			</label>
-		</form>
-
-		<form>
-			<h3>2. 나는 내 삶의 관계적 측면에 대해 만족한다.</h3>
+    </Card>
+	<Card type="inner" title="2. 나는 내 삶의 관계적 측면에 대해 만족한다.">
 			<label>
 				1 <input type= "radio" value="1" name="satis_2" checked={satis_2 === "1"} onChange={this.onChange}/>
 			</label>
@@ -94,195 +90,180 @@ class Young extends Component {
 			<label>
 				7 <input type= "radio" value="7" name="satis_2" checked={satis_2 === "7"} onChange={this.onChange}/>
 			</label>
-		</form>
+    </Card>
+	<Card type="inner" title="3. 나는 내가 속한 집단에 대해 만족한다.">
+		<label>
+			1 <input type= "radio" value="1" name="satis_3" checked={satis_3 === "1"} onChange={this.onChange}/>
+		</label>
+		<label>
+			2 <input type= "radio" value="2" name="satis_3" checked={satis_3 === "2"} onChange={this.onChange}/>
+		</label>
+		<label>
+			3 <input type= "radio" value="3" name="satis_3" checked={satis_3 === "3"} onChange={this.onChange}/>
+		</label>
+		<label>
+			4 <input type= "radio" value="4" name="satis_3" checked={satis_3 === "4"} onChange={this.onChange}/>
+		</label>
+		<label>
+			5 <input type= "radio" value="5" name="satis_3" checked={satis_3 === "5"} onChange={this.onChange}/>
+		</label>
+		<label>
+			6 <input type= "radio" value="6" name="satis_3" checked={satis_3 === "6"} onChange={this.onChange}/>
+		</label>
+		<label>
+			7 <input type= "radio" value="7" name="satis_3" checked={satis_3 === "7"} onChange={this.onChange}/>
+		</label>
+    </Card>
+  	</Card>
 
-		<form>
-			<h3>3. 나는 내가 속한 집단에 대해 만족한다.</h3>
-			<label>
-				1 <input type= "radio" value="1" name="satis_3" checked={satis_3 === "1"} onChange={this.onChange}/>
-			</label>
-			<label>
-				2 <input type= "radio" value="2" name="satis_3" checked={satis_3 === "2"} onChange={this.onChange}/>
-			</label>
-			<label>
-				3 <input type= "radio" value="3" name="satis_3" checked={satis_3 === "3"} onChange={this.onChange}/>
-			</label>
-			<label>
-				4 <input type= "radio" value="4" name="satis_3" checked={satis_3 === "4"} onChange={this.onChange}/>
-			</label>
-			<label>
-				5 <input type= "radio" value="5" name="satis_3" checked={satis_3 === "5"} onChange={this.onChange}/>
-			</label>
-			<label>
-				6 <input type= "radio" value="6" name="satis_3" checked={satis_3 === "6"} onChange={this.onChange}/>
-			</label>
-			<label>
-				7 <input type= "radio" value="7" name="satis_3" checked={satis_3 === "7"} onChange={this.onChange}/>
-			</label>
-		</form>
-	</div>
+  <Card title="감정 질문입니다" style={{marginBottom:'30px'}}>
+    <Card type="inner" title="1. 나는 현재 즐거운 감정을 느끼고 있다.">
+		<label>
+			1 <input type= "radio" value="1" name="emo_1" checked={emo_1 === "1"} onChange={this.onChange}/>
+		</label>
+		<label>
+			2 <input type= "radio" value="2" name="emo_1" checked={emo_1 === "2"} onChange={this.onChange}/>
+		</label>
+		<label>
+			3 <input type= "radio" value="3" name="emo_1" checked={emo_1 === "3"} onChange={this.onChange}/>
+		</label>
+		<label>
+			4 <input type= "radio" value="4" name="emo_1" checked={emo_1 === "4"} onChange={this.onChange}/>
+		</label>
+		<label>
+			5 <input type= "radio" value="5" name="emo_1" checked={emo_1 === "5"} onChange={this.onChange}/>
+		</label>
+		<label>
+			6 <input type= "radio" value="6" name="emo_1" checked={emo_1 === "6"} onChange={this.onChange}/>
+		</label>
+		<label>
+			7 <input type= "radio" value="7" name="emo_1" checked={emo_1 === "7"} onChange={this.onChange}/>
+		</label>
+    </Card>
+	<Card type="inner" title="2. 나는 현재 편안한 감정을 느끼고 있다.">
+		<label>
+			1 <input type= "radio" value="1" name="emo_2" checked={emo_2 === "1"} onChange={this.onChange}/>
+		</label>
+		<label>
+			2 <input type= "radio" value="2" name="emo_2" checked={emo_2 === "2"} onChange={this.onChange}/>
+		</label>
+		<label>
+			3 <input type= "radio" value="3" name="emo_2" checked={emo_2 === "3"} onChange={this.onChange}/>
+		</label>
+		<label>
+			4 <input type= "radio" value="4" name="emo_2" checked={emo_2 === "4"} onChange={this.onChange}/>
+		</label>
+		<label>
+			5 <input type= "radio" value="5" name="emo_2" checked={emo_2 === "5"} onChange={this.onChange}/>
+		</label>
+		<label>
+			6 <input type= "radio" value="6" name="emo_2" checked={emo_2 === "6"} onChange={this.onChange}/>
+		</label>
+		<label>
+			7 <input type= "radio" value="7" name="emo_2" checked={emo_2 === "7"} onChange={this.onChange}/>
+		</label>
+    </Card>
+	<Card type="inner" title="3. 나는 내가 행복한 감정을 느끼고 있다.">
+		<label>
+			1 <input type= "radio" value="1" name="emo_3" checked={emo_3 === "1"} onChange={this.onChange}/>
+		</label>
+		<label>
+			2 <input type= "radio" value="2" name="emo_3" checked={emo_3 === "2"} onChange={this.onChange}/>
+		</label>
+		<label>
+			3 <input type= "radio" value="3" name="emo_3" checked={emo_3 === "3"} onChange={this.onChange}/>
+		</label>
+		<label>
+			4 <input type= "radio" value="4" name="emo_3" checked={emo_3 === "4"} onChange={this.onChange}/>
+		</label>
+		<label>
+			5 <input type= "radio" value="5" name="emo_3" checked={emo_3 === "5"} onChange={this.onChange}/>
+		</label>
+		<label>
+			6 <input type= "radio" value="6" name="emo_3" checked={emo_3 === "6"} onChange={this.onChange}/>
+		</label>
+		<label>
+			7 <input type= "radio" value="7" name="emo_3" checked={emo_3 === "7"} onChange={this.onChange}/>
+		</label>
+    </Card>
+	<Card type="inner" title="4. 나는 현재 부정적인 감정을 느끼고 있다.">
+		<label>
+			1 <input type= "radio" value="1" name="emo_4" checked={emo_4 === "1"} onChange={this.onChange}/>
+		</label>
+		<label>
+			2 <input type= "radio" value="2" name="emo_4" checked={emo_4 === "2"} onChange={this.onChange}/>
+		</label>
+		<label>
+			3 <input type= "radio" value="3" name="emo_4" checked={emo_4 === "3"} onChange={this.onChange}/>
+		</label>
+		<label>
+			4 <input type= "radio" value="4" name="emo_4" checked={emo_4 === "4"} onChange={this.onChange}/>
+		</label>
+		<label>
+			5 <input type= "radio" value="5" name="emo_4" checked={emo_4 === "5"} onChange={this.onChange}/>
+		</label>
+		<label>
+			6 <input type= "radio" value="6" name="emo_4" checked={emo_4 === "6"} onChange={this.onChange}/>
+		</label>
+		<label>
+			7 <input type= "radio" value="7" name="emo_4" checked={emo_4 === "7"} onChange={this.onChange}/>
+		</label>
+    </Card>
+	<Card type="inner" title="5. 나는 현재 짜증나는 감정을 느끼고 있다.">
+		<label>
+			1 <input type= "radio" value="1" name="emo_5" checked={emo_5 === "1"} onChange={this.onChange}/>
+		</label>
+		<label>
+			2 <input type= "radio" value="2" name="emo_5" checked={emo_5 === "2"} onChange={this.onChange}/>
+		</label>
+		<label>
+			3 <input type= "radio" value="3" name="emo_5" checked={emo_5 === "3"} onChange={this.onChange}/>
+		</label>
+		<label>
+			4 <input type= "radio" value="4" name="emo_5" checked={emo_5 === "4"} onChange={this.onChange}/>
+		</label>
+		<label>
+			5 <input type= "radio" value="5" name="emo_5" checked={emo_5 === "5"} onChange={this.onChange}/>
+		</label>
+		<label>
+			6 <input type= "radio" value="6" name="emo_5" checked={emo_5 === "6"} onChange={this.onChange}/>
+		</label>
+		<label>
+			7 <input type= "radio" value="7" name="emo_5" checked={emo_5 === "7"} onChange={this.onChange}/>
+		</label>
+    </Card>
+	<Card type="inner" title="6. 나는 현재 무기력한 감정을 느끼고 있다.">
+		<label>
+			1 <input type= "radio" value="1" name="emo_6" checked={emo_6 === "1"} onChange={this.onChange}/>
+		</label>
+		<label>
+			2 <input type= "radio" value="2" name="emo_6" checked={emo_6 === "2"} onChange={this.onChange}/>
+		</label>
+		<label>
+			3 <input type= "radio" value="3" name="emo_6" checked={emo_6 === "3"} onChange={this.onChange}/>
+		</label>
+		<label>
+			4 <input type= "radio" value="4" name="emo_6" checked={emo_6 === "4"} onChange={this.onChange}/>
+		</label>
+		<label>
+			5 <input type= "radio" value="5" name="emo_6" checked={emo_6 === "5"} onChange={this.onChange}/>
+		</label>
+		<label>
+			6 <input type= "radio" value="6" name="emo_6" checked={emo_6 === "6"} onChange={this.onChange}/>
+		</label>
+		<label>
+			7 <input type= "radio" value="7" name="emo_6" checked={emo_6 === "7"} onChange={this.onChange}/>
+		</label>
+    </Card>
+  </Card>
 
-	<div className="emo_qna">
-	<label>
-		감정질문입니다
-	</label>
-	<h1>결과 2 합 : {result2}</h1>
-		<form>
-			<h3>1. 나는 현재 즐거운 감정을 느끼고 있다.</h3>
-			<label>
-				1 <input type= "radio" value="1" name="emo_1" checked={emo_1 === "1"} onChange={this.onChange}/>
-			</label>
-			<label>
-				2 <input type= "radio" value="2" name="emo_1" checked={emo_1 === "2"} onChange={this.onChange}/>
-			</label>
-			<label>
-				3 <input type= "radio" value="3" name="emo_1" checked={emo_1 === "3"} onChange={this.onChange}/>
-			</label>
-			<label>
-				4 <input type= "radio" value="4" name="emo_1" checked={emo_1 === "4"} onChange={this.onChange}/>
-			</label>
-			<label>
-				5 <input type= "radio" value="5" name="emo_1" checked={emo_1 === "5"} onChange={this.onChange}/>
-			</label>
-			<label>
-				6 <input type= "radio" value="6" name="emo_1" checked={emo_1 === "6"} onChange={this.onChange}/>
-			</label>
-			<label>
-				7 <input type= "radio" value="7" name="emo_1" checked={emo_1 === "7"} onChange={this.onChange}/>
-			</label>
-		</form>
-
-		<form>
-			<h3>2. 나는 현재 편안한 감정을 느끼고 있다.</h3>
-			<label>
-				1 <input type= "radio" value="1" name="emo_2" checked={emo_2 === "1"} onChange={this.onChange}/>
-			</label>
-			<label>
-				2 <input type= "radio" value="2" name="emo_2" checked={emo_2 === "2"} onChange={this.onChange}/>
-			</label>
-			<label>
-				3 <input type= "radio" value="3" name="emo_2" checked={emo_2 === "3"} onChange={this.onChange}/>
-			</label>
-			<label>
-				4 <input type= "radio" value="4" name="emo_2" checked={emo_2 === "4"} onChange={this.onChange}/>
-			</label>
-			<label>
-				5 <input type= "radio" value="5" name="emo_2" checked={emo_2 === "5"} onChange={this.onChange}/>
-			</label>
-			<label>
-				6 <input type= "radio" value="6" name="emo_2" checked={emo_2 === "6"} onChange={this.onChange}/>
-			</label>
-			<label>
-				7 <input type= "radio" value="7" name="emo_2" checked={emo_2 === "7"} onChange={this.onChange}/>
-			</label>
-		</form>	
-
-		<form>
-			<h3>3. 나는 내가 행복한 감정을 느끼고 있다.</h3>
-			<label>
-				1 <input type= "radio" value="1" name="emo_3" checked={emo_3 === "1"} onChange={this.onChange}/>
-			</label>
-			<label>
-				2 <input type= "radio" value="2" name="emo_3" checked={emo_3 === "2"} onChange={this.onChange}/>
-			</label>
-			<label>
-				3 <input type= "radio" value="3" name="emo_3" checked={emo_3 === "3"} onChange={this.onChange}/>
-			</label>
-			<label>
-				4 <input type= "radio" value="4" name="emo_3" checked={emo_3 === "4"} onChange={this.onChange}/>
-			</label>
-			<label>
-				5 <input type= "radio" value="5" name="emo_3" checked={emo_3 === "5"} onChange={this.onChange}/>
-			</label>
-			<label>
-				6 <input type= "radio" value="6" name="emo_3" checked={emo_3 === "6"} onChange={this.onChange}/>
-			</label>
-			<label>
-				7 <input type= "radio" value="7" name="emo_3" checked={emo_3 === "7"} onChange={this.onChange}/>
-			</label>
-		</form>
-		<form>
-			<h3>4. 나는 현재 부정적인 감정을 느끼고 있다.</h3>
-			<label>
-				1 <input type= "radio" value="1" name="emo_4" checked={emo_4 === "1"} onChange={this.onChange}/>
-			</label>
-			<label>
-				2 <input type= "radio" value="2" name="emo_4" checked={emo_4 === "2"} onChange={this.onChange}/>
-			</label>
-			<label>
-				3 <input type= "radio" value="3" name="emo_4" checked={emo_4 === "3"} onChange={this.onChange}/>
-			</label>
-			<label>
-				4 <input type= "radio" value="4" name="emo_4" checked={emo_4 === "4"} onChange={this.onChange}/>
-			</label>
-			<label>
-				5 <input type= "radio" value="5" name="emo_4" checked={emo_4 === "5"} onChange={this.onChange}/>
-			</label>
-			<label>
-				6 <input type= "radio" value="6" name="emo_4" checked={emo_4 === "6"} onChange={this.onChange}/>
-			</label>
-			<label>
-				7 <input type= "radio" value="7" name="emo_4" checked={emo_4 === "7"} onChange={this.onChange}/>
-			</label>
-		</form>
-
-		<form>
-			<h3>5. 나는 현재 짜증나는 감정을 느끼고 있다.</h3>
-			<label>
-				1 <input type= "radio" value="1" name="emo_5" checked={emo_5 === "1"} onChange={this.onChange}/>
-			</label>
-			<label>
-				2 <input type= "radio" value="2" name="emo_5" checked={emo_5 === "2"} onChange={this.onChange}/>
-			</label>
-			<label>
-				3 <input type= "radio" value="3" name="emo_5" checked={emo_5 === "3"} onChange={this.onChange}/>
-			</label>
-			<label>
-				4 <input type= "radio" value="4" name="emo_5" checked={emo_5 === "4"} onChange={this.onChange}/>
-			</label>
-			<label>
-				5 <input type= "radio" value="5" name="emo_5" checked={emo_5 === "5"} onChange={this.onChange}/>
-			</label>
-			<label>
-				6 <input type= "radio" value="6" name="emo_5" checked={emo_5 === "6"} onChange={this.onChange}/>
-			</label>
-			<label>
-				7 <input type= "radio" value="7" name="emo_5" checked={emo_5 === "7"} onChange={this.onChange}/>
-			</label>
-		</form>
-
-		<form>
-			<h3>6. 나는 현재 무기력한 감정을 느끼고 있다.</h3>
-			<label>
-				1 <input type= "radio" value="1" name="emo_6" checked={emo_6 === "1"} onChange={this.onChange}/>
-			</label>
-			<label>
-				2 <input type= "radio" value="2" name="emo_6" checked={emo_6 === "2"} onChange={this.onChange}/>
-			</label>
-			<label>
-				3 <input type= "radio" value="3" name="emo_6" checked={emo_6 === "3"} onChange={this.onChange}/>
-			</label>
-			<label>
-				4 <input type= "radio" value="4" name="emo_6" checked={emo_6 === "4"} onChange={this.onChange}/>
-			</label>
-			<label>
-				5 <input type= "radio" value="5" name="emo_6" checked={emo_6 === "5"} onChange={this.onChange}/>
-			</label>
-			<label>
-				6 <input type= "radio" value="6" name="emo_6" checked={emo_6 === "6"} onChange={this.onChange}/>
-			</label>
-			<label>
-				7 <input type= "radio" value="7" name="emo_6" checked={emo_6 === "7"} onChange={this.onChange}/>
-			</label>
-		</form>
-	</div>
 	{/* 항목 선택 덜 했을 시 팝업창 띄우는 것 추가하면 좋을 듯 */}
-		<Button as="input" type="submit" value="Submit" href={toResult()}
+		<Button as="input" type="submit" value="Submit" style={{marginTop:'10px'}}
 		onClick={()=> {
 			this.props.history.push(toResult())
 		}
-	}
-		style={{marginTop:'10px'}}
+		}
+		
 		/>
 		
 	</div>
