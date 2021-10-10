19575826ba4824e64a4e76b21f16ed8b13a713f6
knyoon <kim_na_yoon@naver.com>
2021-10-11 03:47:10 +0900

    ADD: 로그인 페이지

--- src/index.js
+++ src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {persistStore} from "redux-persist";
+import {store} from "./redux/store";
+import {Provider} from "react-redux";
+import {PersistGate} from "redux-persist/integration/react";
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
+  <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </PersistGate>
+  </Provider>,
   document.getElementById('root')
 );
 
