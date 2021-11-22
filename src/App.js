631cc9d387181a8d03bebd79ba80fb4cf611ebd7
최숙 <langgel1222@gmail.com>
2021-11-22 13:16:45 +0900

    UPDATE : 안 쓰는 페이지 삭제

--- src/App.js
+++ src/App.js
@@ -30,16 +30,12 @@ import ReviewFormCome from "./pages/reviewform/sub/comeForm";
 import LoginFormMidam from "./pages/reviewform/form_sub/formmidam";
 import ReviewFormMidam from "./pages/reviewform/form_sub/formmidamreview";
 
-import LoginFormGamoon from "./pages/reviewform/form_sub/FormGamoon";
 import ReviewFormGamoon from "./pages/reviewform/form_sub/FormGamoonReview";
-import LoginFormMasi from "./pages/reviewform/form_sub/FormMasi";
 import ReviewFormMasi from "./pages/reviewform/form_sub/FormMasiReview";
-import LoginFormDear from "./pages/reviewform/form_sub/FormDear";
 import ReviewFormDear from "./pages/reviewform/form_sub/FormDearReview";
-import LoginFormMonster from "./pages/reviewform/form_sub/FormMonster";
 import ReviewFormMonster from "./pages/reviewform/form_sub/FormMonsterReview";
-import LoginFormGatmael from "./pages/reviewform/form_sub/FormGatmael";
 import ReviewFormGatmael from "./pages/reviewform/form_sub/FormGatmaelReview";
+
 
 import ReviewFormtest from "./pages/reviewform/form_sub/B1_Form";
 import ReviewCheck from "./pages/reviewcheck/reviewList.tsx";
@@ -96,17 +92,15 @@ function App() {
             <Route path="/review/see/form/come" component={ReviewFormCome}></Route>
             <Route path="/review/see/form/midam" component={LoginFormMidam}></Route>
             <Route path="/review/see/form/midam-write" component={ReviewFormMidam}></Route>
-            <Route path="/review/see/form/gamoon" component={LoginFormGamoon}></Route>
+
             <Route path="/review/see/form/gamoon-write" component={ReviewFormGamoon}></Route>
-            <Route path="/review/see/form/gatmael" component={LoginFormGatmael}></Route>
             <Route path="/review/see/form/gatmael-write" component={ReviewFormGatmael}></Route>
-            <Route path="/review/see/form/masi" component={LoginFormMasi}></Route>
             <Route path="/review/see/form/masi-write" component={ReviewFormMasi}></Route>
-            <Route path="/review/see/form/dear" component={LoginFormDear}></Route>
             <Route path="/review/see/form/dear-write" component={ReviewFormDear}></Route>
-            <Route path="/review/see/form/monster" component={LoginFormMonster}></Route>
             <Route path="/review/see/form/monster-write" component={ReviewFormMonster}></Route>
-            
+
+
+
 
           <Route path="/surveyIntro" component={SurveyIntro}></Route>
         </Switch>
